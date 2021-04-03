c764b365e0386d5118b251ca86c94996b445d029
carlos silvestri <carlossilvestri9@gmail.com>
2021-04-03 16:54:19 -0400

    Consultas pero en la misma pag

--- src/components/Barra.js
+++ src/components/Barra.js
@@ -1,53 +1,71 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
-
 const Barra = () => {
-    const history = useHistory();
-    const p = history.location.pathname;
-    const [textoBarra, settextoBarra] = useState('');
-    useEffect(() => {
-        const textoDeLaBarra = (path ='') => {
-            switch(path){
-                case '/':
-                    settextoBarra('ALGUNAS EMPRESAS DE ENVÍO');
-                break;
-                case '/aviso-legal':
-                    settextoBarra('AVISO LEGAL');
-                break;
-                case '/politica-privacidad':
-                    settextoBarra('Política de Privacidad');
-                break;
-                case '/politica-coockies':
-                    settextoBarra('Política de Cookies');
-                  break;
-                case '/contacto':
-                    settextoBarra('Contacto');
-                break;
-                default:
-                    settextoBarra('');
-                break;
-            }
-        }
-        textoDeLaBarra(p);
-    }, [p]);
-    // console.log(p);
+  const history = useHistory();
+  const p = history.location.pathname;
+  const [textoBarra, settextoBarra] = useState("");
+  let TRNum = `
+  <div id="TRNum"></div>
+  `;
+  useEffect(() => {
+    const textoDeLaBarra = (path = "") => {
+      switch (path) {
+        case "/":
+          settextoBarra("ALGUNAS EMPRESAS DE ENVÍO");
+          break;
+        case "/aviso-legal":
+          settextoBarra("AVISO LEGAL");
+          break;
+        case "/politica-privacidad":
+          settextoBarra("Política de Privacidad");
+          break;
+        case "/politica-coockies":
+          settextoBarra("Política de Cookies");
+          break;
+        case "/contacto":
+          settextoBarra("Contacto");
+          break;
+        default:
+          settextoBarra("");
+          break;
+      }
+    };
+    textoDeLaBarra(p);
+  }, [p]);
+  // console.log(p);
   return (
     <Fragment>
-      <nav className="com-breadcrumbs">
-        <div className="com-breadcrumbs__wrapper">
-          <ol className="com-breadcrumbs__items">
-            <li className="com-breadcrumbs__item">
-              <Link to={"/"} itemProp="item">
-                <span itemProp="name">Buscapack</span>
-              </Link>
-            </li>
-            <li className="com-breadcrumbs__item">
-              <span itemProp="name">{textoBarra}</span>
-            </li>
-          </ol>
-        </div>
-      </nav>
+      <div>
+        <nav className="com-breadcrumbs">
+          <div className="com-breadcrumbs__wrapper">
+            <ol className="com-breadcrumbs__items">
+              <div>
+                <li className="com-breadcrumbs__item">
+                  <Link to={"/"} itemProp="item">
+                    <span itemProp="name">Buscapack</span>
+                  </Link>
+                </li>
+                <li className="com-breadcrumbs__item">
+                  <span itemProp="name">{textoBarra} </span>
+                </li>
+              </div>
+              <div>
+                <li className="com-breadcrumbs__item">
+                  <div className="alert alert-warning" role="alert" style={{marginBottom: '0px'}}>
+                    ¿Contactar a la empresa de envíos? Te ayudamos da
+                    click
+                    <button type="button" className="btn btn-primary" style={{marginLeft: '10px'}}>
+                      aquí
+                    </button>
+                  </div>
+                </li>
+              </div>
+            </ol>
+          </div>
+        </nav>
+        <div dangerouslySetInnerHTML={{__html: TRNum}}></div>
+      </div>
     </Fragment>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,69 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { css } from "@emotion/react";
 import buscapackFondoBlanco from "../static/img/buscapck-logo-blanco.svg";
+// import axios from "axios";
 
 const Header = () => {
-  const [numEnvio, setnumEnvio] = useState('');
-
+  // const [numEnvio, setnumEnvio] = useState("");
+  let formularioHTML = `
+  <form class="com-searchbox__form com-searchbox__form--tracking"
+        role="form" action="//track.trackingmore.com" method="get" onsubmit="return false"
+        >
+          <div class="com-searchbox__input-wrapper com-searchbox__input-wrapper--tracking com-labeled-input">
+            <!-- <label for="searchbox-tracking">Número de envío Buscapack</label> -->
+            <input type="text"
+              class="com-input com-searchbox__input--tracking"
+              required
+              value=""
+              id="button_tracking_number"
+              name="button_tracking_number" 
+              placeholder="Número de Seguimiento de Envío"
+              name="num"/>
+          </div>
+          <input type="hidden" name="lang" value="es" />
+          <input id="button_express_code" type="hidden" name="lang" value="" />
+          <button id="query" onclick="return doTrack()" type="submit" class="com-searchbox__submit com-searchbox__submit--tracking">Buscar</button>
+        </form>
+  `;
   // Funciones
-  const irATrackingmorePage = ()=>{
-    if(numEnvio !== ''){
+  /*const irATrackingmorePage = () => {
+    if (numEnvio !== "") {
       // console.log('irATrackingmorePage ', numEnvio);
       window.location = "https://www.trackingmore.com/es/" + numEnvio;
     }
-  }
+  };*/
+  /*useEffect(() => {
+    const testApi = async () => {
+      try {
+        const url = `https://api.trackingmore.com/v3/trackings/courier`;
+        const result = await axios.get(url, {
+          headers: {
+            "Tracking-Api-Key": "6c43fcfc-653a-4dc4-8c1a-9fe19d330a9b",
+          },
+        });
+        console.log("result  ", result);
+      } catch (error) {
+        console.log("There was a mistake: ", error);
+      }
+    };
+
+    // testApi();
+  }, []);*/
+
   return (
     <Fragment>
-      <div
-        className="com-cub-hero  has-searchbox-unfolded imagenFondo"
-      >
+      <div>
+      <div className="com-cub-hero  has-searchbox-unfolded imagenFondo">
         <header className="com-header ">
           <div className="com-header__logo">
             <Link className="" to={"/"}>
               <img
                 src={buscapackFondoBlanco}
                 alt="buscapck-logo-blanco"
-                css={css`width: 160px;`}
+                css={css`
+                  width: 160px;
+                `}
               />
             </Link>
           </div>
@@ -34,7 +73,7 @@ const Header = () => {
               <li className="com-header__menu-item">
                 <a
                   className="com-header__menu-item-link"
-                  href="https://www.packlink.es/empresas-de-transporte/"
+                  href="https://www.trackingmore.com/carriers.html"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
@@ -42,10 +81,7 @@ const Header = () => {
                 </a>
               </li>
               <li className="com-header__menu-item com-header__menu-item--help">
-                <Link
-                  className="com-header__menu-item-link"
-                  to={"/contacto"}
-                >
+                <Link className="com-header__menu-item-link" to={"/contacto"}>
                   Contacto
                 </Link>
               </li>
@@ -53,41 +89,41 @@ const Header = () => {
           </nav>
         </header>
         <div className="com-cub-hero__wrapper">
-            <h1 className="com-cub-hero__moto">Localizador, tracking y seguimiento de envíos nacionales e internacionales</h1>
-         </div>
-         <div className="com-searchbox">
-            <section className="com-searchbox__wrapper">
-               <header className="com-searchbox__header">
-                  <ul className="com-searchbox__tabber">
-                     <li className="com-searchbox__tab com-searchbox__tab--track is-active" >
-                        <Link className="com-searchbox__tab-text com-searchbox__tab-text--desktop" to={"#"}>Seguimiento</Link>
-                        <Link className="com-searchbox__tab-text com-searchbox__tab-text--mobile" to={"#"}>Seguimiento</Link>
-                     </li>
-                  </ul>
-               </header>
-               <div className="com-searchbox__tab-content-wrapper">
-                  <section className="com-searchbox__tab-content">
-                  </section>
-                  {/* <!-- NUMERO DE ENVIO --> */}
-                  <section className="com-searchbox__tab-content is-active">
-                     <div className="com-searchbox__form com-searchbox__form--tracking">
-                        <div className="com-searchbox__input-wrapper com-searchbox__input-wrapper--tracking com-labeled-input">
-                           {/* <!-- <label for="searchbox-tracking">Número de envío Buscapack</label> --> */}
-                           <input type="text"
-                              className="com-input com-searchbox__input--tracking"
-                              required
-                              defaultValue=""
-                              onChange={e => setnumEnvio(e.target.value)}
-                              id="searchbox-tracking"
-                              placeholder="Número de Seguimiento de Envío"
-                              name="num"/>
-                        </div>
-                        <button type="button" className="com-searchbox__submit com-searchbox__submit--tracking" onClick={ irATrackingmorePage }>Buscar</button>
-                     </div>
-                  </section>
-               </div>
-            </section>
-         </div>
+          <h1 className="com-cub-hero__moto">
+            Localizador, tracking y seguimiento de envíos nacionales e
+            internacionales
+          </h1>
+        </div>
+        <div className="com-searchbox">
+          <section className="com-searchbox__wrapper">
+            <header className="com-searchbox__header">
+              <ul className="com-searchbox__tabber">
+                <li className="com-searchbox__tab com-searchbox__tab--track is-active">
+                  <Link
+                    className="com-searchbox__tab-text com-searchbox__tab-text--desktop"
+                    to={"#"}
+                  >
+                    Seguimiento
+                  </Link>
+                  <Link
+                    className="com-searchbox__tab-text com-searchbox__tab-text--mobile"
+                    to={"#"}
+                  >
+                    Seguimiento
+                  </Link>
+                </li>
+              </ul>
+            </header>
+            <div className="com-searchbox__tab-content-wrapper">
+              <section className="com-searchbox__tab-content"></section>
+              {/* <!-- NUMERO DE ENVIO --> */}
+              <section className="com-searchbox__tab-content is-active">
+                <div dangerouslySetInnerHTML={{__html: formularioHTML}}></div>
+              </section>
+            </div>
+          </section>
+        </div>
+      </div>
       </div>
     </Fragment>
   );
